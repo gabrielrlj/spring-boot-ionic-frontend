3ac49f9a197ce608db4a2943c9f458bc91cc577b
Gabriel Jardim <gabrielrlj99@outlook.com>
2020-06-17 10:51:13 -0300

    mostrando as categorias

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,13 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { CategoriaService } from '../../services/domain/categoria.service';
-
-/**
- * Generated class for the CategoriasPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { CategoriaDTO } from '../../models/categoria.dto';
+import { API_CONFIG } from '../../config/api.config';
 
 @IonicPage()
 @Component({
@@ -16,6 +11,10 @@ import { CategoriaService } from '../../services/domain/categoria.service';
 })
 export class CategoriasPage {
 
+
+  items : CategoriaDTO[];
+  bucketUrl : string = API_CONFIG.bucketBaseUrl;
+
   constructor(public navCtrl: NavController, public navParams: NavParams, 
     public categoriaService : CategoriaService) {
   }
@@ -23,7 +22,7 @@ export class CategoriasPage {
   ionViewDidLoad() {
     this.categoriaService.findAll()
       .subscribe(response => {
-        console.log(response);
+        this.items = response;
       },
       error => {
         console.log(error);
