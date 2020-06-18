71682bc118afcec930ba4a126a73a6488a749f33
Gabriel Jardim <gabrielrlj99@outlook.com>
2020-06-18 10:33:38 -0300

    fazendo o app aproveitar o usuario logado na tela inicial

--- src/services/domain/auth.service.ts
+++ src/services/domain/auth.service.ts
@@ -29,6 +29,16 @@ export class AuthService{
         };
         this.storage.setLocalUser(usr);
     }
+
+    refreshToken() {
+        return this.http.post(
+            `${API_CONFIG.baseUrl}/auth/refresh_token`, 
+            {},
+            {
+                observe: 'response',
+                responseType: 'text'
+            });
+    }
     
     logout(){
         this.storage.setLocalUser(null);
